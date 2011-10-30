bddc2d86934af69c4eeb7458d88b9b8448031d4d
Pau Fernández <pau.fernandez@upc.edu>
2011-10-30 19:35:22 +0100

    Bug fix, 'usage' didn't display

--- 06-config-zookeeper-node.sh
+++ 06-config-zookeeper-node.sh
@@ -8,7 +8,7 @@ GATEWAY="192.168.1.1"
 # Parse command line
 
 if [ $# -lt 2 ]; then
-   usage: "storm-config.sh <number> <maxnumber> [<net-interface>]"
+   echo "usage: storm-config.sh <number> <maxnumber> [<net-interface>]"
    exit 1
 fi
 
